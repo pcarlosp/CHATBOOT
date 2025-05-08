6db5edf54b2e6e2db2bec34888329f952e1cf872
pcarlosp <pablopizolotto@gmail.com>
2025-05-08 15:58:12 -0300

    Delete chat_engine.py

--- chat_engine.py
+++
@@ -1,7 +0,0 @@
-def responder(mensagem):
-    if "oi" in mensagem.lower():
-        return "Olá! Em que posso ajudar?"
-    elif "ajuda" in mensagem.lower():
-        return "Claro! Qual sua dúvida?"
-    else:
-        return "Mensagem recebida. Alguém da equipe responderá em breve."
